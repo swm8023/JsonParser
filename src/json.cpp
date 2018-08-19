f93ffc5168480954843b34ac21965adf9b9e0b9b
suweimin <swm8023@gmail.com>
2018-08-19 10:01:39 +0800

    base

--- src/json.cpp
+++ src/json.cpp
@@ -1,7 +1,24 @@
 #include "parser.hpp"
+#include <string>
+#include <iostream>
+#include <boost/spirit/home/x3.hpp>
 
+std::string json_str = R"({
+    "a": "bb"
+})"; 
 
 int main(int argc, char *argv[]) {
-
+    namespace x3 = boost::spirit::x3;
+    using json::grammer::value_root;
+    json::ast::value_root root;
+    std::string::const_iterator iter = json_str.begin();
+    std::string::const_iterator end = json_str.end();
+    bool r = x3::phrase_parse(iter, end, value_root, x3::ascii::space, root);
+    // if (r && iter == end) {
+    //     std::cout << "Success!!" << std::endl;
+    // } else {
+    //     std::string context(iter, iter + 30 > end ? end : iter+30);
+    //     std::cout << "Failed!  " << context << std::endl;
+    // }
 
 } 
--- src/parser.hpp
+++ src/parser.hpp
@@ -4,54 +4,73 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <boost/config/warning_disable.hpp>
 #include <boost/spirit/home/x3.hpp>
-#include <boost/spirit/home/x3/support/utility/annotate_on_success.hpp>
-#include <boost/spirit/home/x3/support/ast/position_tagged.hpp>
 #include <boost/spirit/home/x3/support/ast/variant.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/std_pair.hpp>
+#include <boost/fusion/include/io.hpp>
 
 
-namespace json { namespace parser {
+namespace json { namespace ast {
     namespace x3 = boost::spirit::x3;
 
-    namespace ast {
-        struct value;
-        typedef std::map<std::string, value> object;
-        typedef std::vector<value> array;
-        struct value : x3::variant<std::string, int, float, bool, object, array> {
-            using base_type::base_type;
-            using base_type::operator=;
-        };
-        typedef std::pair<std::string, value> key_value_pair;
+    // struct value;
+    // typedef std::map<std::string, value> object;
+    // typedef std::vector<value> array;
+    // typedef std::pair<std::string, value> kv_pair;
 
-        struct value_root: x3::position_tagged {
-            value entries;
-        };
-    }
+    struct value : x3::variant<std::string, int> {
+        using base_type::base_type;
+        using base_type::operator=;
+    };
 
-    
+    struct value_root {
+        value root;
+    };
+
+}}
+
+BOOST_FUSION_ADAPT_STRUCT(json::ast::value_root, root)
+
+namespace json {namespace grammer {
+    namespace x3 = boost::spirit::x3;    
+    namespace ascii = boost::spirit::x3::ascii;
+
+    using x3::lit;
+    using x3::lexeme;
+
+    using ascii::char_;
+    using ascii::string;
     // rule ids
-    struct _value_cont_class;
-    struct _value_class;
+    struct value_root_class {};
+    struct value_class {};
+    struct object_class {};
+    struct array_class {};
+    struct kv_pair_class {};
     
     // rules
-    x3::rule<_value_cont_class, ast::value_root> const _value_root = "_value_root";
-    x3::rule<_value_class, ast::value> const _value = "_value";
-    
+    x3::rule<value_root_class, ast::value_root> const value_root = "value_root";
+    x3::rule<value_class, ast::value> const value = "value";
+    // x3::rule<object_class, ast::object> const object = "object";
+    // x3::rule<array_class, ast::array> const array = "array";
+    // x3::rule<kv_pair_class, ast::kv_pair> const kv_pair = "kv_pair";
 
     // grammer
-    auto const _value_def = 
+    auto const quoted_string = x3::lexeme['"' >> *(x3::char_ - '"') >> '"'];
+    auto const value_def = quoted_string;
+    // auto const kv_pair_def = quoted_string >> '=' >> value;
+    // auto const object_def = '{' >> *kv_pair >> '}';
+    auto const value_root_def = value;
 
-
-    BOOST_SPIRIT_DEFINE
-
-    
+    // BOOST_SPIRIT_DEFINE(value_root, value, object, kv_pair);
+    BOOST_SPIRIT_DEFINE(value_root, value);
 
     
 }}
 
 
-BOOST_FUSION_ADAPT_STRUCT(json::parser::ast::value_root, entries)
+
+
 
 #endif // _JSON_PARSER_HPP
