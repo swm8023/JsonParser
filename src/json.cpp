fadd0381f3d0a9730d28d98d64e827fde25c4acb
suweimin <swm8023@gmail.com>
2018-08-19 16:05:50 +0800

    ok

--- src/json.cpp
+++ src/json.cpp
@@ -1,24 +1,30 @@
 #include "parser.hpp"
 #include <string>
 #include <iostream>
-#include <boost/spirit/home/x3.hpp>
 
 std::string json_str = R"({
-    "a": "bb" 
-})"; 
+    "a": ["bb", "c"] 
+})";
+
+
 
 int main(int argc, char *argv[]) {
     namespace x3 = boost::spirit::x3;
-    using json::grammer::value_root;
-    json::ast::value_root root;
-    std::string::const_iterator iter = json_str.begin();
-    std::string::const_iterator end = json_str.end();
-    bool r = x3::phrase_parse(iter, end, value_root, x3::ascii::space, root);
-    // if (r && iter == end) {
-    //     std::cout << "Success!!" << std::endl;
-    // } else {
-    //     std::string context(iter, iter + 30 > end ? end : iter+30);
-    //     std::cout << "Failed!  " << context << std::endl;
-    // }
-
+    using json::grammer::value;
+    json::ast::value root;
+    auto iter = json_str.begin();
+    auto end = json_str.end();
+    using boost::spirit::x3::ascii::space;
+    bool r = x3::phrase_parse(iter, end, value, space, root);
+    if (r && iter == end) {
+        std::cout << "Success!!" << std::endl;
+        json::ast::value_printer printer;
+        boost::apply_visitor(printer(), root);
+    } else {
+        std::string context(iter, iter + 30 > end ? end : iter+30);
+        std::cout << "Failed!  " << context << std::endl;
+    }
+   
 } 
+
+
--- src/parser.hpp
+++ src/parser.hpp
@@ -4,72 +4,100 @@
 #include <string>
 #include <map>
 #include <vector>
-#include <boost/config/warning_disable.hpp>
+#include <iostream>
+
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/home/x3/support/ast/variant.hpp>
+
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/std_pair.hpp>
 #include <boost/fusion/include/io.hpp>
 
 
-namespace json { namespace ast {
+namespace json {
+
     namespace x3 = boost::spirit::x3;
+    namespace ast {
+        struct value;
 
-    // struct value;
-    // typedef std::map<std::string, value> object;
-    // typedef std::vector<value> array;
-    // typedef std::pair<std::string, value> kv_pair;
+        struct null_t {};
 
-    struct value : x3::variant<std::string, int> {
-        using base_type::base_type;
-        using base_type::operator=;
-    };
+        using object = std::map<std::string, value>;
+        using array = std::vector<value>;
+        
+        using object_member = std::pair<std::string, value>;
 
-    struct value_root {
-        value root;
-    };
+        struct value : x3::variant<std::string, object, array> {
+            using base_type::base_type;
+            using base_type::operator=;
+        };
 
-}}
+        int const tabsize = 4;
 
-BOOST_FUSION_ADAPT_STRUCT(json::ast::value_root, root)
+        struct value_printer
+        {
+            typedef void result_type;
 
-namespace json {namespace grammer {
-    namespace x3 = boost::spirit::x3;    
-    namespace ascii = boost::spirit::x3::ascii;
+            value_printer(int indent = 0) : indent(indent) {}
 
-    using x3::lit;
-    using x3::lexeme;
+            void operator()(object const& cont) const
+            {
+                std::cout << '{' << std::endl;
+                for (auto const& entry : cont)
+                {
+                    tab(indent+tabsize);
+                    std::cout << '"' << entry.first << "\" = ";
+                    boost::apply_visitor(value_printer(indent+tabsize), entry.second);
+                }
+                tab(indent);
+                std::cout << '}' << std::endl;
+            }
 
-    using ascii::char_;
-    using ascii::string;
-    // rule ids
-    struct value_root_class {};
-    struct value_class {};
-    struct object_class {};
-    struct array_class {};
-    struct kv_pair_class {};
+            void operator()(std::string const& text) const
+            {
+                std::cout << '"' << text << '"' << std::endl;
+            }
+
+            void tab(int spaces) const
+            {
+                for (int i = 0; i < spaces; ++i)
+                    std::cout << ' ';
+            }
+
+            int indent;
+        };
+    }
+
+    namespace grammer {
+        struct value_class;
+        struct object_class;
+        struct array_class;
+        struct object_member_class;
+
+        x3::rule<value_class, ast::value> const value{"value"};
+        x3::rule<object_class, ast::object> const object{"object"};
+        x3::rule<array_class, ast::array> const array{"array"};
+        x3::rule<object_member_class, ast::object_member> const object_member{"object_member"};
+
+        auto const quote_string = 
+            x3::lexeme['"' >> *(x3::char_ - '"') >> '"'];
+
+        auto const value_def = 
+            quote_string | object | array;
+
+        auto const object_member_def = 
+            quote_string >> ':' >> value;
+        
+        auto const object_def = 
+            x3::lit('{') >>  -(object_member % ',') >> x3::lit('}');
     
-    // rules
-    x3::rule<value_root_class, ast::value_root> const value_root = "value_root";
-    x3::rule<value_class, ast::value> const value = "value";
-    // x3::rule<object_class, ast::object> const object = "object";
-    // x3::rule<array_class, ast::array> const array = "array";
-    // x3::rule<kv_pair_class, ast::kv_pair> const kv_pair = "kv_pair";
+        auto const array_def =
+            x3::lit('[') >>  -(value % ',') >> x3::lit(']');
 
-    // grammer
-    auto const quoted_string = x3::lexeme['"' >> *(x3::char_ - '"') >> '"'];
-    auto const value_def = quoted_string;
-    // auto const kv_pair_def = quoted_string >> '=' >> value;
-    // auto const object_def = '{' >> *kv_pair >> '}';
-    auto const value_root_def = value;
+        BOOST_SPIRIT_DEFINE(value, object, array, object_member)
+    }
 
-    // BOOST_SPIRIT_DEFINE(value_root, value, object, kv_pair);
-    BOOST_SPIRIT_DEFINE(value_root, value);
-
-    
-}}
-
-
+}
 
 
 
